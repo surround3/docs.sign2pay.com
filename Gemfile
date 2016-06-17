e010bddfa6d23ef7e2131fffcf764f4b09b8cb7c
nicklloyd <nick@nicklloyd.co>
2016-06-17 15:57:56 +0200

    update middleman and remove deprecated livereload

--- Gemfile
+++ Gemfile
@@ -4,9 +4,6 @@ source 'https://rubygems.org'
 
 gem "middleman", "~>3.4.1"
 
-# Live-reloading plugin
-gem "middleman-livereload", "~> 3.3.0"
-
 # For faster file watcher updates on Windows:
 gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
 
--- config.rb
+++ config.rb
@@ -35,10 +35,6 @@
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
 
-# Reload the browser automatically whenever files change
-configure :development do
-  activate :livereload
-end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
